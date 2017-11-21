1eea31f934e907510b37b1852809a014c1d2c344
Lily <836876398@qq.com>
2017-11-21 15:05:25 +0800

    loop

--- image_slider/demo.js
+++ image_slider/demo.js
@@ -5,33 +5,44 @@ var nextBtn = document.getElementsByClassName('next')[0];
 var btnUl   = document.getElementsByTagName('ul')[1];
 var btnLi   = btnUl.getElementsByTagName('li');
 
-var ulLeft  = parseInt(imgUl.style.left);
+var ulLeft  = parseInt(imgUl.style.left) || 0;
 var liWidth = 400;
 var ulWidth = 0;
 var index   = 0;
+var loop    = false;
 
-// imgUl.innerHTML += imgUl.innerHTML;     //  无缝滚动
+if (loop) {
+    var choneFirst = imgLi[0].cloneNode(true);
+    var cloneLast  = imgLi[imgLi.length - 1].cloneNode(true);
+
+    imgUl.appendChild(choneFirst);
+    imgUl.insertBefore(cloneLast, imgLi[0]);
+
+    ulLeft = -liWidth;
+    imgUl.style.left = ulLeft + 'px';
+}
+
 ulWidth = imgLi.length * liWidth;
 imgUl.style.width = ulWidth + 'px';  //  使得ul够宽
 
 prevBtn.addEventListener('click', function () {
-    if (index == 0) {
+    if (!loop && index == 0) {
         return false;
     }
 
     index--;
-    buttonShow();
     animate(liWidth);
+    buttonShow();
 }, false)
 
 nextBtn.addEventListener('click', function () {
-    if (index == 4) {
+    if (!loop && index == imgLi.length - 1) {
         return false;
     }
     
     index++;
-    buttonShow();
     animate(-liWidth);
+    buttonShow();
 }, false)
 
 btnUl.addEventListener('click', function (e) {
@@ -45,6 +56,13 @@ btnUl.addEventListener('click', function (e) {
 }, false)
 
 function buttonShow () {
+    if (loop && index > imgLi.length - 3) {
+        index = 0
+    }
+    if (index < 0) {
+        index = imgLi.length - 3
+    }
+
     for (var i = 0, len = btnLi.length; i < len; i++) {
         btnLi[i].className = '';
     }
@@ -53,6 +71,15 @@ function buttonShow () {
 }
 
 function animate (offset) {
+    if (loop && ulLeft == 0) {
+        ulLeft = -(liWidth * (imgLi.length - 2))
+    }
+
+    if (loop && ulLeft == -(liWidth * (imgLi.length - 1))) {
+        ulLeft = -liWidth;
+    }
+
+
     ulLeft += offset;
     imgUl.style.left = ulLeft + 'px';
 }
