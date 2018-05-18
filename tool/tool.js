dc5a72fd6350dd25a377ac53f19bd1e7f879c063
Lily <836876398@qq.com>
2018-05-18 17:06:55 +0800

    更新工具函数库

--- tool/tool.js
+++ tool/tool.js
@@ -447,3 +447,55 @@ function encodeHtmlStr (str) {
 
     return str;
 }
+
+/**
+ * 移除 url中的某个参数
+ * @param url
+ * @param parameter
+ * @returns {*}
+ */
+function removeURLParameter (url, parameter) {
+    //prefer to use l.search if you have a location/link object
+    var urlparts= url.split('?');
+    if (urlparts.length>=2) {
+
+        var prefix= encodeURIComponent(parameter)+'=';
+        var pars= urlparts[1].split(/[&;]/g);
+
+        //reverse iteration as may be destructive
+        for (var i= pars.length; i-- > 0;) {
+            //idiom for string.startsWith
+            if (pars[i].lastIndexOf(prefix, 0) !== -1) {
+                pars.splice(i, 1);
+            }
+        }
+
+        url= urlparts[0] + (pars.length > 0 ? '?' + pars.join('&') : "");
+        return url;
+    } else {
+        return url;
+    }
+}
+
+/**
+ * 获取url中的某个参数
+ * @param name
+ * @param url
+ * @returns {*}
+ */
+function getParameterByName (name, url) {
+    if (!url) {
+        url = window.location.href;
+    }
+    name        = name.replace(/[\[\]]/g, "\\$&");
+    var regex   = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+    var results = regex.exec(url);
+
+    if (!results) {
+        return null;
+    }
+    if (!results[2]) {
+        return '';
+    }
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
